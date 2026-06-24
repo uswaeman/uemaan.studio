048e9a53f587b72a4b31d78e26d144edf064d10a
uenaturals <uenaturals1@gmail.com>
2026-06-24 23:43:26 +0500

    Add GitHub Pages deployment workflow

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
 
+const redirectPath = new URLSearchParams(window.location.search).get('p');
+
+if (redirectPath) {
+  const normalizedPath = redirectPath.startsWith('/') ? redirectPath : `/${redirectPath}`;
+  window.history.replaceState(null, '', normalizedPath);
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
