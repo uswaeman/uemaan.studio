931ce03be011e27bea58ab7d4e7de31694ac66ca
uenaturals <uenaturals1@gmail.com>
2026-06-25 13:42:53 +0500

    Deploy site assets to root for Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,20 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'src/main.tsx',
+      output: {
+        entryFileNames: 'assets/app.js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name?.endsWith('.css')) {
+            return 'assets/app.css';
+          }
+
+          return 'assets/[name][extname]';
+        },
+      },
+    },
+  },
 });
